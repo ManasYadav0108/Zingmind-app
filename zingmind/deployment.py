123789ea585b8734c3aad493f6a3a04fd614940f
ManasYadav0108 <manasy04@gmail.com>
2025-10-30 13:32:58 +0530

    fix deployment

--- zingmind/deployment.py
+++ zingmind/deployment.py
@@ -1,9 +1,12 @@
 import os 
 from .settings import *
 from .settings import BASE_DIR
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['*','https://'+ os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
 
